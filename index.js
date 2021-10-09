fb11ad3616ae9cbe7c1b2b8f2933daac3078aedb
christina makatura <cnmak@comcast.net>
2021-10-09 01:19:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replaceAll(/[^A-z0-9' -]|\^/g, '')
+  }
+
+  static titleize(title){
+  if(!title) {
+    return '';
+  }
+  let arr = [];
+  let excludeWords = ['of', 'the', 'and', 'a', 'an', 'but', 'for', 'at', 'by', 'from']
+  arr = title.split(' ');
+  return arr.map((word, i) =>
+  {
+     return excludeWords.includes(word) && i > 0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+
+  }).join(' ');
 }
+}
+
+
+
+/*
+   const ignore = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+   string = string.toLowerCase().split(' ');
+  for (var i = 0; i < string.length; i++) {
+    if (ignore.contains(string)){
+      string
+    } else {
+    string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1); 
+  }
+  string.join(' ');
+}
+}
+
+
+}
+
+*/
